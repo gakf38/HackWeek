ee5dcbc2af2c4bb8b803456ee14aa6c50dc3c2b1
William Jack Boening <wjbz82@mail.missouri.edu>
2015-11-17 13:37:54 -0600

    edit database test

--- SocketChatv2/index.js
+++ SocketChatv2/index.js
@@ -1,36 +1,42 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var cookieParser = require('cookie-parser');
-
-app.use(cookieParser());
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
-
-	res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
-	console.log("Cookies: ", req.cookies);
 });
 
-io.on('connection', function(socket){
-	var greetMsg = 'A user has connected'; 
-	io.emit('chat message', greetMsg);
-	console.log('a user connected');
-		socket.on('chat message', function(msg){
 
+io.on('connection', function(socket){
+	var greetMsg = 'A user has connected';
+	io.emit('chat message', greetMsg);
+	console.log("user connected");
+	socket.on('chat message', function(msg){
 		if(msg.length == 0){
 			console.log("msg too short");
 		}else{
 			io.emit('chat message', msg);
 		}
 	});
-});
 
-io.on('disconnect', function(){
-	var leavingMsg = 'A user has left';
-	io.emit('chat message', leavingMsg);
-	console.log("user has left");
-	console.log("a user has left");
+	socket.on('addToDB', function(object){
+
+		MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
+			
+			if(err)
+			throw err;
+			else
+			console.log("connected");
+
+		var collection = db.collection("posts");
+		collection.insert(object, function(err, docs){
+			collection.count(function(err, count){
+				console.log(format("count = %s", count));
+			});
+		});
+		});
+	});
 });
 
 http.listen(3000, function(){
