784468ebb2eb079905188c9a3d70692e37f69d37
William Jack Boening <wjbz82@mail.missouri.edu>
2015-11-20 17:45:13 -0600

    adding up/down
    function to each button; only works on
    page refresh tho
    i believe the function to auto-update is in place, i just cant get it to
    work myself

--- new_src/index.js
+++ new_src/index.js
@@ -62,30 +62,152 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('updateDBUP', function(id){
+		
 		MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
 			if(err)
 				throw err;
 			else
 				console.log("update");
 
-			/* --------------REMOVE THIS CODE BELOW--------------- */
 			var temp1, temp2, collection; 
 			collection = db.collection("posts");
-			temp1 = collection.find({"id":id});
-			temp2 = collection.find({"id":id++});
-			console.log(collection.find({"id":id}));
-			
-			/* -----------ADD THE CODE I UPLOADED HERE------------ */
 
+			var cursor = collection.find({"id":id});
+			cursor.each(function(err, doc){
+				if(err)
+					throw err;
+				else
+					console.log("all good");
+
+				if(doc != null){
+					temp1 = doc;
+				}else{
+					console.log("there was an issue");
+				}
+			});
+
+			var cursor2 = collection.find({"id":id + 1});
+			cursor2.each(function(err, doc){
+				if(err)
+					throw err;
+				else
+					console.log("all good");
+
+				if(doc != null){
+					temp2 = doc;
+
+					//this is fantastic news
+
+					var temp3; //super temp variable that isn't really needed but am making it for general use
+					temp3 = temp1.content; //save temp2's content so we can update in a sec
+
+					collection.update({"id":id}, {$set: {"content":temp2.content}});
+					collection.update({"id":id + 1}, {$set: {"content":temp3}});
+
+					var posts = []; 
+
+					//gather posts to send back the user; i think it will
+					//be the easiest if we use a different socket method here
+					//so i can do a better type of comparison
+					//because in reality the list would be the same minus 2 posts?
+					//im not sure on this part to be honest
+
+					//actually i'll just send the list to the .html page
+					//and will gather up the current posts
+					//and iterate over them, and if the currentPost that im on != currntPost on .html
+					//don't match, i will force them to match
+
+					collection.find({"type":"post"}, {_id:0}).each(function(err, doc){
+						if(err)
+							throw err;
+						else
+							console.log("no error!!");
+
+						if(doc != null)
+							posts.push(doc);
+
+						io.emit('updateList', posts);
+					});
+
+				}else{
+					console.log("there was an issue");
+				}
+			});
 		});
 	});
 
-	socket.on('updateDBDOWN', function(id){
+socket.on('updateDBDOWN', function(id){
+		
 		MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
 			if(err)
 				throw err;
 			else
-				console.log("test");
+				console.log("update");
+
+			var temp1, temp2, collection; 
+			collection = db.collection("posts");
+
+			var cursor = collection.find({"id":id});
+			cursor.each(function(err, doc){
+				if(err)
+					throw err;
+				else
+					console.log("all good");
+
+				if(doc != null){
+					temp1 = doc;
+				}else{
+					console.log("there was an issue");
+				}
+			});
+
+			var cursor2 = collection.find({"id":id - 1});
+			cursor2.each(function(err, doc){
+				if(err)
+					throw err;
+				else
+					console.log("all good");
+
+				if(doc != null){
+					temp2 = doc;
+
+					//this is fantastic news
+
+					var temp3; //super temp variable that isn't really needed but am making it for general use
+					temp3 = temp1.content; //save temp2's content so we can update in a sec
+
+					collection.update({"id":id}, {$set: {"content":temp2.content}});
+					collection.update({"id":id - 1}, {$set: {"content":temp3}});
+
+					var posts = []; 
+
+					//gather posts to send back the user; i think it will
+					//be the easiest if we use a different socket method here
+					//so i can do a better type of comparison
+					//because in reality the list would be the same minus 2 posts?
+					//im not sure on this part to be honest
+
+					//actually i'll just send the list to the .html page
+					//and will gather up the current posts
+					//and iterate over them, and if the currentPost that im on != currntPost on .html
+					//don't match, i will force them to match
+
+					collection.find({"type":"post"}, {_id:0}).each(function(err, doc){
+						if(err)
+							throw err;
+						else
+							console.log("no error!!");
+
+						if(doc != null)
+							posts.push(doc);
+
+						io.emit('updateList', posts);
+					});
+
+				}else{
+					console.log("there was an issue");
+				}
+			});
 		});
 	});
 
